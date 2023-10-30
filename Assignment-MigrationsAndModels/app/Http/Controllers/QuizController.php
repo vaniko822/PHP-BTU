e245795dce6fca0c17dbc82bc48c3f8d935a8cdb
vaniko822 <vano.kukutchanashvili.1@btu.edu.ge>
2023-10-30 12:34:10 +0400

    CRUD

--- Assignment-MigrationsAndModels/app/Http/Controllers/QuizController.php
+++ Assignment-MigrationsAndModels/app/Http/Controllers/QuizController.php
@@ -11,4 +11,29 @@ class QuizController extends Controller
         $quizzes = Quiz::all();
         return view('main_page', ['quizzes' => $quizzes]);
     }
+
+    public function createOrUpdate(Request $request){
+        $data = $request->post();
+        $quizz = [
+            'name' => $data['name'],
+            'description' => $data['description']
+        ];
+
+        if(isset($data['id'])){
+            Quiz::where('id', $data['id'])->update($quizz);
+        } else {
+            Quiz::create($quizz);
+        }
+
+        return redirect('/');
+    }
+
+    public function savePage(string $id = NULL){
+        if($id){
+            $quizz = Quiz::find($id);
+            return view('save_quizz', ['quizz' => $quizz]);
+        }
+
+        return view('save_quizz');
+    }
 }
